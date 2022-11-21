5f65bda2a0ca5148827d2c41e466898e0bbbe248
Barzin <barzin.development@gmail.com>
2022-11-21 15:13:52 +0100

    Part 4: Shopping cart=> Add a new array to the state of your Container component: 'shoppingListItems'. This array will be updated when we add and remove items from the cart.

--- my-groceries-app/src/Container.js
+++ my-groceries-app/src/Container.js
@@ -14,6 +14,7 @@ class Container extends Component {
                 {id: 4, title : "Noodles (hot ones)" },
                 {id: 5, title : "Ayran" }
             ],
+            shoppingListItems: [],
         }
     }
 
